0d60c4129e9974f07c30023d8314c0a72cd0eb35
신가은 <sge7102@naver.com>
2019-12-06 00:15:25 +0900

    initial commit

---
+++ termProject/src/test/java/com/example/termProject/TermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.termProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
